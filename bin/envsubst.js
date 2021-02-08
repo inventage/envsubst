9d97304625190c756c6434f7fdfffe31e629b71e
Peter Siska <psiska@inventage.com>
2021-02-08 22:27:22 +0100

    Initial implementation

---
+++ bin/envsubst.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+const path = require('path');
+const meow = require('meow');
+const globby = require('globby');
+const replace = require('replace-in-file');
+const Table = require('cli-table3');
+
+const cli = meow(
+  `
+  Usage
+    $ envsubst <glob>
+
+  Examples
+    $ envsubst 'dist/**/*.js'
+`
+);
+
+(async () => {
+  const files = await globby(cli.input);
+  const replacements = [];
+
+  await replace({
+    files,
+    from: /\${(\w+)(:-(\w+))?}/gm,
+    // dry: true,
+    to: (...args) => {
+      const [string, name, , fallback] = args;
+      const { length, [length - 1]: filename } = args;
+
+      // Check if the found variable name is available in process.env
+      const value = process.env[name] || fallback;
+      if (!value) {
+        return string;
+      }
+
+      replacements.push({
+        filename,
+        name,
+        value,
+      });
+
+      return value;
+    },
+  });
+
+  if (replacements.length < 1) {
+    console.log('No variable replacements made.');
+    return;
+  }
+
+  const table = new Table({
+    head: ['File', 'Variable', 'Value'],
+    style: {
+      // No colors
+      head: [],
+      border: [],
+    },
+  });
+
+  console.info(`Made ${replacements.length} replacement${replacements.length > 1 ? 's' : ''}:`);
+  replacements.forEach(replacement => {
+    const { filename, name, value } = replacement;
+    table.push([filename, name, value]);
+  });
+  console.info(table.toString());
+})();
