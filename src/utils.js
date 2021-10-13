ce55c2dd5fd86a1f190a75af067947b58f591aad
Peter Siska <psiska@inventage.com>
2021-10-13 08:49:10 +0200

    Add `replaceVarsSync` function (sync version of the promise based `replaceVars` function).

--- src/utils.js
+++ src/utils.js
@@ -19,34 +19,48 @@ const variableRegexPattern = (prefix = '') => {
  * @param {string} string
  * @param {object} variables
  * @param {string} prefix
- * @returns {Promise<string>}
+ * @returns {Promise<[string]>}
  */
 const replaceVars = (string, variables = {}, prefix = '') =>
   new Promise(resolve => {
-    const regex = new RegExp(variableRegexPattern(prefix), 'gm');
-    const matches = [...string.matchAll(regex)];
-
-    let replaced = string;
-    const replacements = [];
-    for (const match of matches) {
-      const [original, name, , fallback] = match;
-      const value = Object.hasOwnProperty.call(variables || {}, name) ? variables[name] : fallback;
-      if (value !== undefined) {
-        const replacement = replacements.find(r => r.from === original && r.to === value);
-        if (replacement) {
-          replacement.count = replacement.count + 1;
-        } else {
-          replacements.push({ from: original, to: value, count: 1 });
-        }
-
-        replaced = replaced.split(original).join(value);
-      }
-    }
-
-    resolve([replaced, replacements]);
+    resolve(replaceVarsSync(string, variables, prefix));
   });
+
+/**
+ * Replaces all variable placeholders in the given string with either variable values
+ * found in the variables parameter OR with the given default in the variable string.
+ *
+ * @param {string} string
+ * @param {object} variables
+ * @param {string} prefix
+ * @returns {[string]}
+ */
+const replaceVarsSync = (string, variables = {}, prefix = '') => {
+  const regex = new RegExp(variableRegexPattern(prefix), 'gm');
+  const matches = [...string.matchAll(regex)];
+
+  let replaced = string;
+  const replacements = [];
+  for (const match of matches) {
+    const [original, name, , fallback] = match;
+    const value = Object.hasOwnProperty.call(variables || {}, name) ? variables[name] : fallback;
+    if (value !== undefined) {
+      const replacement = replacements.find(r => r.from === original && r.to === value);
+      if (replacement) {
+        replacement.count = replacement.count + 1;
+      } else {
+        replacements.push({ from: original, to: value, count: 1 });
+      }
+
+      replaced = replaced.split(original).join(value);
+    }
+  }
+
+  return [replaced, replacements];
+};
 
 module.exports = {
   variableRegexPattern,
   replaceVars,
+  replaceVarsSync,
 };
