ee469d59279d563f0426097f72d717bd7b2cf43d
Peter Siska <psiska@inventage.com>
2021-10-13 07:59:44 +0200

    Rename `mocks` directory to `fixtures` in tests

--- test/envsubst.test.js
+++ test/envsubst.test.js
@@ -7,13 +7,13 @@ const pkg = require('../package.json');
 const dotenv = require('dotenv');
 
 const REPO_DIR = path.resolve(__dirname, '..');
-const MOCKS_DIR = path.resolve(__dirname, 'mocks');
+const FIXTURES_DIR = path.resolve(__dirname, 'fixtures');
 const TEMP_DIR = path.resolve(__dirname, '__tmp');
 const CLI_FILE = `${REPO_DIR}/bin/envsubst.js`;
 
-// Copy mocks into a temporary directory
+// Copy fixtures into a temporary directory
 fse.mkdirsSync(TEMP_DIR);
-fse.copySync(MOCKS_DIR, TEMP_DIR);
+fse.copySync(FIXTURES_DIR, TEMP_DIR);
 
 // Cleanup by removing the temporary directory after we're done
 afterAll(() => {
@@ -60,24 +60,24 @@ describe('envsubst CLI execution', () => {
     });
   });
 
-  const mockDirs = fs
+  const fixturesDir = fs
     .readdirSync(TEMP_DIR)
     .map(file => path.resolve(TEMP_DIR, file))
     .filter(path => fs.lstatSync(path).isDirectory());
 
-  mockDirs.forEach(mockDir => {
-    const mockBasename = path.basename(mockDir);
-    const given = path.resolve(mockDir, `${mockBasename}`);
-    const envFile = path.resolve(mockDir, `${mockBasename}.env`);
-    const expected = path.resolve(mockDir, `${mockBasename}_expected`);
+  fixturesDir.forEach(fixtureDir => {
+    const fixtureBasename = path.basename(fixtureDir);
+    const given = path.resolve(fixtureDir, `${fixtureBasename}`);
+    const envFile = path.resolve(fixtureDir, `${fixtureBasename}.env`);
+    const expected = path.resolve(fixtureDir, `${fixtureBasename}_expected`);
 
-    it('all mock files exists in mock directory "${mockBasename}"', () => {
+    it(`all fixture files exists in fixture directory "${fixtureBasename}"`, () => {
       expect(fs.existsSync(given)).toEqual(true);
       expect(fs.existsSync(envFile)).toEqual(true);
       expect(fs.existsSync(expected)).toEqual(true);
     });
 
-    it.only(`replaces variables in mock "${mockBasename}"`, done => {
+    it.only(`replaces variables in fixture "${fixtureBasename}"`, done => {
       dotenv.config({ path: envFile });
       const child = spawn('node', [CLI_FILE, given], {
         env: process.env,
