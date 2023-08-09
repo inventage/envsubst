3b0c499bebc7162b2b2ecce2213ceaac44b7b08e
Peter Siska <peter.siska@inventage.com>
2023-08-09 10:11:20 +0200

    Add more codestyle plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,13 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:unicorn/recommended', 'prettier'],
-  plugins: ['import'],
+  plugins: ['import', 'no-only-tests', 'ava', 'require-extensions'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   env: { node: true },
   rules: {
+    'no-only-tests/no-only-tests': 'error',
     'import/extensions': ['error', 'ignorePackages'],
   },
   overrides: [],
